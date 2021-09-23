92ab42fa1e78e7325095d573ac389804929ea50f
Johnson <yuhang0229@gmail.com>
2021-09-23 21:25:38 +1000

    adding detail page

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,19 +1,71 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable arrow-body-style */
-import React from 'react';
-import { RouteComponentProps } from 'react-router';
-// import styles from './detailPage.module.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { RouteComponentProps, useParams } from 'react-router';
+import { Spin, Row, Col } from 'antd';
+import styles from './detailPage.module.css';
+import { Header, Footer } from '../../components';
 
 interface MatchParams {
   touristRouteId: string;
 }
 
-export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (props) => {
-  const { match: { params } } = props;
+export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = () => {
+  const { touristRouteId } = useParams<MatchParams>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(
+          `http://123.56.149.216:8089/api/touristRoutes/${touristRouteId}`,
+        );
+        setProduct(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Spin
+        size="large"
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '100%',
+        }}
+      />
+    );
+  }
+
+  if (error) {
+    return <div>网站出错：{error}</div>;
+  }
+
   return (
-    <h1>
-      旅游路线详情页面, ID:
-      {params.touristRouteId}
-    </h1>
+    <>
+      <Header />
+      <div className={styles['page-content']}>
+        {/* 产品简介 与 日期选择 */}
+        <div className={styles['product-intro-container']}></div>
+        {/* 锚点菜单 */}
+        <div className={styles['product-detail-anchor']}></div>
+        {/* 产品特色 */}
+        <div id='feature' className={styles['product-detail-container']}></div>
+        <div id='fees'></div>
+      </div>
+      <Footer />
+    </>
   );
 };
